c59c9c15e62eea2154b8cb596b3ccf18246f9b99
delcisp <delcianesouzapinheiro@gmail.com>
2024-04-18 16:27:48 -0400

    router settings

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,84 +1,128 @@
 "use client";
-import React, {useState} from 'react';
-import ProjectsCards from './ProjectsCards';
-import ProjectTag from './ProjectTag';
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
 
-const ProjectsData = [
-    {
-      id: 1, 
-      title: "Oka Website",
-      description: "etc etc etc",
-      image: "/images/projects/oka_project.png",
-      tag: ["All", "Web"],
-      gitUrl: "github.com/delcisp",
-      previewUrl: "github.com/delcisp"
-    },
-    {
-        id: 2, 
-        title: "Oka Website",
-        description: "etc etc etc",
-        image: "/images/projects/oka_project.png",
-        tag: ["All", "Web"],
-        gitUrl: "github.com/delcisp",
-        previewUrl: "github.com/delcisp"
-      },
-      {
-        id: 3, 
-        title: "Oka Website",
-        description: "etc etc etc",
-        image: "/images/projects/oka_project.png",
-        tag: ["All", "Web"],
-        gitUrl: "github.com/delcisp",
-        previewUrl: "github.com/delcisp"
-      },
-      {
-        id: 4, 
-        title: "Oka Website",
-        description: "etc etc etc",
-        image: "/images/projects/oka_project.png",
-        tag: ["All", "Web"],
-        gitUrl: "github.com/delcisp",
-        previewUrl: "github.com/delcisp"
-      },
-      {
-        id: 5, 
-        title: "Oka Website",
-        description: "etc etc etc",
-        image: "/images/projects/oka_project.png",
-        tag: ["All", "Web"],
-        gitUrl: "github.com/delcisp",
-        previewUrl: "github.com/delcisp"
-      },
-]
-
+const projectsData = [
+  {
+    id: 1,
+    title: "Pomodoro",
+    description: "Project 1 description",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Banco de Talentos",
+    description: "Project 2 description",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3, 
+    title: "Oka Website",
+    description: "etc etc etc",
+    image: "/images/projects/oka_project.png",
+    tag: ["All", "Web"],
+    gitUrl: "github.com/delcisp",
+    previewUrl: "github.com/delcisp"
+  },
+  {
+    id: 4,
+    title: "Sistema de Reconhecimento Facial",
+    description: "Project 4 description",
+    image: "/images/projects/4.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Crud com React",
+    description: "Authentication and CRUD operations",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Full-stack Roadmap",
+    description: "Project 5 description",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
 
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
   const handleTagChange = (newTag) => {
     setTag(newTag);
-  }
+  };
 
-  const filteredProjects = ProjectsData.filter((project) => {
-    	project.tag.includes(tag);
-  } )
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
   return (
-    <>
-    <h2>Meus projetos</h2>
-    <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
-      <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All" } />
-      <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web" } />
-      <ProjectTag onClick={handleTagChange} name="Desktop" isSelected={tag === "Desktop" } />
-    </div>
-    <div> {ProjectsData.map((project) => <ProjectsCards key={project.id}
-     title={project.title} description={project.description} imgUrl={project.image}
-     gitUrl={project.gitUrl}
-     previewUrl={project.previewUrl}
-     
-     /> )} </div>
-    </>
-  )
-}
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default ProjectsSection;
